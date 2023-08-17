6aa775e8da1724c03dadefca1cde9d87277011cc
Nicolò Boschi <boschi1997@gmail.com>
2023-08-17 16:32:05 +0200

    Use 0.0.2-alpha and add completion instructions

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # LangStream CLI
+
+
+## Installation
+
+```
+brew tap LangStream/langstream
+brew install langstream
+```
+
+## Usage
+
+```
+langstream -h
+```
+## Autocompletions
+### ZSH
+
+```
+[[ $(grep 'langstream generate-completion' "$HOME/.zshrc") ]] || echo -e "source <(langstream generate-completion)" >> "$HOME/.zshrc"
+source $HOME/.zshrc # or open another terminal
+```
+### Bash
+
+```
+[[ $(grep 'langstream generate-completion' "$HOME/.bashrc") ]] || echo -e "source <(langstream generate-completion)" >> "$HOME/.bashrc"
+source $HOME/.bashrc # or open another terminal
+```
+
+
+
--- langstream.rb
+++ langstream.rb
@@ -1,13 +1,12 @@
 class Langstream < Formula
     desc "The LangStream project combines the intelligence of large language models with the agility of streaming processing, to create powerful processing applications."
     homepage "https://docs.langstream.ai/"
-    url "https://github.com/LangStream/langstream/releases/download/test-nicolo/langstream-cli-0.0.2-SNAPSHOT.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "a218b5708abeca9cbeddd476fb547c92e206d9505a0716a4653f6106515a4847"
+    url "https://github.com/LangStream/langstream/releases/download/0.0.2-alpha/langstream-cli-0.0.2-alpha.zip"
+    sha256 "cdb867cd39f10b1afecfb7ffb5419761a9fadaf6cd0cccdb6a8d34c6868e276d"
     head "https://github.com/LangStream/langstream.git"
   
     def install
         libexec.install Dir["*"]
         bin.install_symlink libexec/"bin/langstream"
-    end
-  
+    end  
   end
