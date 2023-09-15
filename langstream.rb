a3bb80c5666a018557d26ee9752f9540699fb40c
Nicolò Boschi <boschi1997@gmail.com>
2023-09-15 16:42:03 +0200

    Update langstream.rb

--- langstream.rb
+++ langstream.rb
@@ -1,8 +1,8 @@
 class Langstream < Formula
     desc "The LangStream project combines the intelligence of large language models with the agility of streaming processing, to create powerful processing applications."
     homepage "https://docs.langstream.ai/"
-    url "https://github.com/LangStream/langstream/releases/download/v0.0.18/langstream-cli-0.0.18.zip"
-    sha256 "af58f13242174e649bb5148479fb79f88c226f8c5c8d0a8fbafeba72f4763152"
+    url "https://github.com/LangStream/langstream/releases/download/v0.0.19/langstream-cli-0.0.19.zip"
+    sha256 "370a2a8130e581a3dbc0f89acf1687b1d700512dbb406082ebadd357d6763359"
     head "https://github.com/LangStream/langstream.git"
   
     def install
