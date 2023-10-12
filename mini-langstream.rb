3bdbeca96592f7cfc1f55f5631826d72e4d3ba5b
Nicolò Boschi <boschi1997@gmail.com>
2023-10-12 09:23:55 +0200

    Update mini-langstream.rb

--- mini-langstream.rb
+++ mini-langstream.rb
@@ -1,8 +1,8 @@
 class MiniLangstream < Formula
     desc "mini-langstream is a local LangStream cluster for development and testing, running on Minikube"
     homepage "https://docs.langstream.ai/"
-    url "https://github.com/LangStream/langstream/releases/download/v0.1.0/mini-langstream-0.1.0.zip"
-    sha256 "fe19ddbb2033444cc32dbac843f27de7ccaa98793553495cb8c9d6a1d7b2207b"
+    url "https://github.com/LangStream/langstream/releases/download/v0.2.0/mini-langstream-0.2.0.zip"
+    sha256 "64cd5ed3c3268955a6542e8cf092d02ccf716481fd71ad1ec263b996e75d0596"
     head "https://github.com/LangStream/langstream.git"
   
     depends_on "minikube"
