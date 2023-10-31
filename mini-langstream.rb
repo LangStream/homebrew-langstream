a43fed6a0ab5549b27188505613c7b9c1197aeef
Nicolò Boschi <boschi1997@gmail.com>
2023-10-31 17:10:13 +0100

    MiniLangstream 0.4.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- mini-langstream.rb
+++ mini-langstream.rb
@@ -1,8 +1,8 @@
 class MiniLangstream < Formula
     desc "mini-langstream is a local LangStream cluster for development and testing, running on Minikube"
     homepage "https://docs.langstream.ai/"
-    url "https://github.com/LangStream/langstream/releases/download/v0.3.0/mini-langstream-0.3.0.zip"
-    sha256 "4e2bcc81eba5c95635b832c6f6ce7820ba7c788fd4fd0d5dacc51999719df08a"
+    url "https://github.com/LangStream/langstream/releases/download/v0.4.0/mini-langstream-0.4.0.zip"
+    sha256 "02bb55b475d9b72979a86b99f08577f77bc9fcf8637f68cb6067d5a03f606c87"
     head "https://github.com/LangStream/langstream.git"
   
     depends_on "minikube"
