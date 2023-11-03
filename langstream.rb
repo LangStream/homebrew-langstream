6a5cda0c2135166bf44b335937751ae4c485a082
Nicolò Boschi <boschi1997@gmail.com>
2023-11-03 13:56:51 +0100

    add openjdk@17 as dependency

--- langstream.rb
+++ langstream.rb
@@ -4,6 +4,8 @@ class Langstream < Formula
     url "https://github.com/LangStream/langstream/releases/download/v0.4.2/langstream-cli-0.4.2.zip"
     sha256 "ad1661b2c33b7ce83ebf7d2166a9d96743f54d44cd449b348c19cea62460de06"
     head "https://github.com/LangStream/langstream.git"
+
+    depends_on "openjdk@17"
   
     def install
         libexec.install Dir["*"]
