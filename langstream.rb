b40c8073d9ddbc01091efc2fa6b732e8c6250992
Nicolò Boschi <boschi1997@gmail.com>
2023-12-12 09:16:41 -0800

    Langstream 0.5.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- langstream.rb
+++ langstream.rb
@@ -1,8 +1,8 @@
 class Langstream < Formula
     desc "The LangStream project combines the intelligence of large language models with the agility of streaming processing, to create powerful processing applications."
     homepage "https://docs.langstream.ai/"
-    url "https://github.com/LangStream/langstream/releases/download/v0.5.3/langstream-cli-0.5.3.zip"
-    sha256 "84cd2756921ceb735b2ae5cff14135f801a5ddcddf57976ee85b66caca57a1ff"
+    url "https://github.com/LangStream/langstream/releases/download/v0.5.4/langstream-cli-0.5.4.zip"
+    sha256 "a2e98108f07ef9a1321b11ec6442abea7de1aa3bdd0b2394e03101fe4c658112"
     head "https://github.com/LangStream/langstream.git"
 
     depends_on "openjdk@17"
